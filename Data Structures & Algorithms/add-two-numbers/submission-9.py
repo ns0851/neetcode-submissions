90c07eb978f70435950f81a2a90081626e26c214
Satyadev <126260629+ns0851@users.noreply.github.com>
2026-07-06 09:39:24 +0530

    Add: add-two-numbers - submission-9

---
+++ Data Structures & Algorithms/add-two-numbers/submission-9.py
@@ -0,0 +1,60 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        carry=0
+        prev=None
+        c=0
+        head=None
+        while l1!=None or l2!=None:
+            if l1!=None and l2!=None:
+                addi = l1.val+l2.val+carry
+            elif l1==None:
+                addi = l2.val+carry
+            else:
+                addi = l1.val+carry
+            carry=0
+
+            newNode = ListNode(addi)
+            if c==0:
+                head=newNode
+                prev=newNode
+            if addi > 9:
+                curr=newNode.val
+                newNode.val=int(str(curr)[1])
+                carry = int(str(curr)[0])
+            if c==0:            
+                prev.next=None
+                c=1
+            else:
+                prev.next=newNode
+                prev=prev.next
+            if l1!=None and l2!=None:
+                l1=l1.next
+                l2=l2.next
+            elif l1==None:
+                l2=l2.next
+            else:
+                l1=l1.next
+        if carry>0:
+            newNode = ListNode(carry)
+            newNode.next=None
+            prev.next = newNode
+        else:
+            prev.next=None
+        
+        # while l1!=None:
+        #     prev.next=l1
+        #     l1=l1.next
+        #     prev = prev.next
+        # while l2!=None:
+        #     prev.next=l2
+        #     l2=l2.next
+        #     prev = prev.next
+
+
+        return head
